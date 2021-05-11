1b81ff95425b6ce977d4eca1f679600bcb169e91
Fabio Bonelli <fb@fabiobonelli.it>
2021-05-11 12:54:02 +0200

    chore: remove superfluous CSS

--- src/components/Wizard.vue
+++ src/components/Wizard.vue
@@ -107,17 +107,6 @@ export default {
 </script>
 
 <style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 .steps {
   margin-bottom: 4rem;
 }
