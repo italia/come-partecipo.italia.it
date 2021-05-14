cabd36417820b0fb9c8c8f514eb8f8c33a63f0da
Fabio Bonelli <fb@fabiobonelli.it>
2021-05-14 16:34:41 +0200

    refactor(lint): add AirBnB rules and enable vue/recommended

--- src/components/Wizard.vue
+++ src/components/Wizard.vue
@@ -1,22 +1,36 @@
 <template>
-  <div class="wizard" v-if="currentChoice">
-
+  <div
+    v-if="currentChoice"
+    class="wizard"
+  >
     <nav
       v-if="activeStep > 0"
-      class="breadcrumb-container" aria-label="breadcrumb"
+      class="breadcrumb-container"
+      aria-label="breadcrumb"
     >
       <ol class="breadcrumb">
-        <li class="breadcrumb-item" v-for="(madeChoice, index) in breadcrumbsChoices" :key="index">
-          <a href="#" @click="moveToStep(index)">{{ truncate(madeChoice.question, 60) }}</a>
+        <li
+          v-for="(madeChoice, index) in breadcrumbsChoices"
+          :key="index"
+          class="breadcrumb-item"
+        >
+          <a
+            href="#"
+            @click="moveToStep(index)"
+          >{{ truncate(madeChoice.question, 60) }}</a>
         </li>
       </ol>
     </nav>
 
-    <div ref="page-content" tabindex="-1">
+    <div
+      ref="page-content"
+      tabindex="-1"
+    >
       <p
         v-if="currentChoice.comment"
         class="h4 pb-4"
-      >{{ currentChoice.comment }}
+      >
+        {{ currentChoice.comment }}
       </p>
 
       <div
@@ -40,7 +54,9 @@
           </div>
         </div>
       </div>
-      <h1 class="h3">{{ currentChoice.question }}</h1>
+      <h1 class="h3">
+        {{ currentChoice.question }}
+      </h1>
 
       <div class="container">
         <div class="row justify-content-center">
@@ -50,19 +66,26 @@
               class="choices"
               aria-label="Scegli una delle opzioni seguenti:"
             >
-              <li v-for="(possibleChoice, index) in currentChoice.content" :key="possibleChoice.label">
+              <li
+                v-for="(possibleChoice, index) in currentChoice.content"
+                :key="possibleChoice.label"
+              >
                 <o-button
                   class="choice"
                   variant="outline-primary"
                   @click="selectChoice(possibleChoice, index)"
-                  >
-                  {{possibleChoice.label}}
+                >
+                  {{ possibleChoice.label }}
                 </o-button>
               </li>
             </ul>
             <div v-else>
-              <h1 class="h3">{{ currentChoice.label }}</h1>
-              <vue-markdown-lite class="reply my-4 p-5">{{ currentChoice.content }}</vue-markdown-lite>
+              <h1 class="h3">
+                {{ currentChoice.label }}
+              </h1>
+              <vue-markdown-lite class="reply my-4 p-5">
+                {{ currentChoice.content }}
+              </vue-markdown-lite>
             </div>
 
             <o-button
@@ -71,7 +94,10 @@
               variant="secondary"
               @click="goBack()"
             >
-              <o-icon class="icon icon-white" icon="arrow-left-circle"></o-icon> Indietro
+              <o-icon
+                class="icon icon-white"
+                icon="arrow-left-circle"
+              /> Indietro
             </o-button>
             <o-button
               v-if="isLeaf(currentChoice)"
@@ -79,7 +105,10 @@
               variant="primary"
               @click="restart()"
             >
-              <o-icon class="icon icon-white" icon="restore"></o-icon> Ricomincia
+              <o-icon
+                class="icon icon-white"
+                icon="restore"
+              /> Ricomincia
             </o-button>
           </div>
         </div>
@@ -90,102 +119,97 @@
 
 <script>
 import treeObj from '@/configuration/tree.yaml';
-import VueMarkdownLite from '@earthtone/vue-markdown-lite'
-import { logAction } from '@/analytics.js'
-
+import VueMarkdownLite from '@earthtone/vue-markdown-lite';
+import { logAction } from '@/analytics';
 
 export default {
   name: 'Wizard',
   components: {
-    'vue-markdown-lite': VueMarkdownLite
+    'vue-markdown-lite': VueMarkdownLite,
   },
-  data () {
+  data() {
     return {
       activeStep: 0,
       choices: [],
-      completed: false
-    }
+      completed: false,
+    };
+  },
+  computed: {
+    currentChoice() {
+      return this.choices[this.choices.length - 1];
+    },
+    breadcrumbsChoices() {
+      const notLeafChoices = this.choices.filter((choice) => !this.isLeaf(choice));
+      return notLeafChoices.splice(0, notLeafChoices.length - 1);
+    },
   },
   mounted() {
-    this.choices.push(treeObj.root)
+    this.choices.push(treeObj.root);
 
-    history.pushState({ choices: this.choices, activeStep: this.activeStep }, '')
+    window.history.pushState({ choices: this.choices, activeStep: this.activeStep }, '');
 
     window.onpopstate = (event) => {
       if (!event.state) {
-        return
+        return;
       }
-      this.choices = event.state.choices
-      this.activeStep = event.state.activeStep
+      this.choices = event.state.choices;
+      this.activeStep = event.state.activeStep;
     };
 
     window.onbeforeunload = () => {
       if (!this.completed) {
         // TRACK DROPOUT
-        logAction('dropout', this.currentChoice.question || this.currentChoice.label)
+        logAction('dropout', this.currentChoice.question || this.currentChoice.label);
       }
-    }
-
-  },
-  computed: {
-    currentChoice() {
-      return this.choices[this.choices.length - 1]
-    },
-    breadcrumbsChoices() {
-      let notLeafChoices = this.choices.filter((choice) => {
-        return !this.isLeaf(choice)
-      })
-      return notLeafChoices.splice(0, notLeafChoices.length - 1)
-    }
+    };
   },
   methods: {
     truncate(text, len) {
-      return text.length > len ? text.slice(0, len) + '…' : text;
+      return text.length > len ? `${text.slice(0, len)}…` : text;
     },
     restart() {
       window.location.reload();
     },
     goBack() {
-      this.moveToStep(this.activeStep - 1)
+      this.moveToStep(this.activeStep - 1);
       this.focusContent();
       if (!this.completed) {
         // TRACK BACK
-        logAction('back', this.currentChoice.question || this.currentChoice.label)
+        logAction('back', this.currentChoice.question || this.currentChoice.label);
       }
     },
     focusContent() {
       this.$nextTick(() => {
-        this.$refs['page-content'].focus()
-      })
+        this.$refs['page-content'].focus();
+      });
     },
     isLeaf(node) {
-      if (typeof(node.content) === 'string') {
-        if(!this.completed) {
-          this.completed = true
+      if (typeof (node.content) === 'string') {
+        if (!this.completed) {
+          this.completed = true;
           // TRACK COMPLETED
-          logAction('completed', this.currentChoice.question || this.currentChoice.label)
+          logAction('completed', this.currentChoice.question || this.currentChoice.label);
         }
-        return true
-      } else {
-        return false
+        return true;
       }
+      return false;
     },
     selectChoice(currentChoice) {
-      this.choices.push(currentChoice)
-      this.activeStep = this.choices.length - 1
-      history.pushState({ choices: this.choices, activeStep: this.activeStep }, '')
+      this.choices.push(currentChoice);
+      this.activeStep = this.choices.length - 1;
+      window.history.pushState({ choices: this.choices, activeStep: this.activeStep }, '');
 
-      this.$emit('choice', currentChoice.question || currentChoice.label)
+      this.$emit('choice', currentChoice.question || currentChoice.label);
 
       /* Focus the newly loaded content to guide assistive technologies */
-      this.focusContent()
+      this.focusContent();
     },
     moveToStep(step) {
-      this.activeStep = step
-      this.choices = this.choices.slice(0, step + 1)
-    }
-  }
-}
+      this.activeStep = step;
+      this.choices = this.choices.slice(0, step + 1);
+    },
+  },
+};
 </script>
 
 <style scoped>
